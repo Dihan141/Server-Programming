c9bd712f44b2c5baf3f16c818ad9fa4587e442b4
Dihan141 <87206556+Dihan141@users.noreply.github.com>
2023-09-05 23:59:43 +0600

    updated

--- Lab 3/app.js
+++ Lab 3/app.js
@@ -1,9 +1,15 @@
 const express = require('express')
-const { logRequest } = require('./middleware')
+const os = require('os')
+const { logRequest, apiKeyMiddleware } = require('./middleware')
 const router = require('./routes')
 const fs = require('fs')
+const PORT = 3000
 const app = express()
 app.use(router)
+
+app.get("/authorization", apiKeyMiddleware, (req, res)=>{
+    res.send("Authorization complete")
+})
 
 
 app.get("/middleware", logRequest, (req, res) => {
@@ -26,16 +32,80 @@ app.get("/data", (req, res)=>{
 })
 
 app.post("/data", (req, res)=>{
-    fs.writeFile("./data.json", req.body, (req, res)=>{
+    fs.writeFile("./data.json", JSON.stringify(req.body), (err)=>{
         if(err){
             res.send(err)
         }
         else{
-            res.send("Write successful")
+            res.send("Write successful!")
         }
     })
 })
 
-app.listen(5000, ()=>{
-    console.log("Listening....")
+app.put("/data", (req, res) => {
+    const newData = req.body;
+
+    fs.readFile("./data.json", "utf-8", (err, data) => {
+        if (err) {
+            res.send(err);
+            return;
+        }
+        
+        const existingData = JSON.parse(data)
+        Object.assign(existingData, newData)
+
+        fs.writeFile("./data.json", JSON.stringify(existingData), (err) => {
+            if (err) {
+                res.send(err);
+                return;
+            }
+
+            res.send("Append successful");
+        });
+    });
+});
+
+app.delete("/data", (req, res)=>{
+    fs.unlink("./data.json", (err)=>{
+        if(err){
+            res.send(err)
+            return
+        }
+
+        res.send("Deleted successfully!!")
+    })
+})
+
+app.get("/os-info", (req, res)=>{
+    fs.readFile("./os-data.json", "utf-8", (err, data)=>{
+        if(err){
+            res.send(err)
+            return
+        }
+
+        if(data){
+            res.send(data)
+            return
+        }
+
+        const osInfo = {
+            name: os.type(),
+            release: os.release(),
+            totalmem: os.totalmem(),
+            freemem: os.freemem()
+        }
+
+        fs.writeFile("./os-data.json", JSON.stringify(osInfo, null, 2), (err)=>{
+            if(err){
+                res.send(err)
+                return
+            }
+
+            res.send("OS info stored")
+        })
+    })
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Listening to port: ${PORT}`)
 })
--- Lab 3/middleware.js
+++ Lab 3/middleware.js
@@ -3,6 +3,15 @@ const logRequest = (req, res, next)=>{
     next()
 }
 
+const apiKeyMiddleware = (req, res, next)=>{
+    const apikey = req.query.apikey
+
+    if(!apikey || apikey != 123){
+        res.status(401).json({error: "You are not authorized!"})
+    }
+}
+
 module.exports = {
-    logRequest
+    logRequest,
+    apiKeyMiddleware
 }
